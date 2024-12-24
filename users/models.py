3d69392cb4ec4eea731ade170ae1613d8eeb7910
fedirky <fedirky@gmail.com>
2024-12-24 14:25:55 +0100

    Added new "watched movies"
    Added history of finished and unfinished movies

--- users/models.py
+++ users/models.py
@@ -10,3 +10,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class MovieWatch(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='movie_watches')
+    is_finished = models.BooleanField(default=False)
+    watched_at = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self):
+        return f'{self.movie.movie_name} - Watched: {self.is_finished}'
+
+
--- users/views.py
+++ users/views.py
@@ -5,5 +5,7 @@ from .models import UserProfile
 @login_required
 def user_profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
-    return render(request, 'users/profile.html', {'profile': user_profile})
 
+    movie_watches = user_profile.movie_watches.all()
+    
+    return render(request, 'users/profile.html', {'profile': user_profile, 'movie_watches': movie_watches})
