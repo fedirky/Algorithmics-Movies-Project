7446b0412581564d13fad15cfe804229e72f78b6
fedirky <fedirky@gmail.com>
2025-01-13 22:20:48 +0200

    Added indexes in sql

--- movies/models.py
+++ movies/models.py
@@ -2,29 +2,27 @@ from django.db import models
 
 
 class MovieGenre(models.Model):
-    name =  models.TextField()
+    name = models.TextField(db_index=True)  # Индекс на поле name
     def __str__(self):
         return self.name
-
 
 class MovieDirector(models.Model):
-    name =  models.TextField()
+    name = models.TextField(db_index=True)  # Индекс на поле name
     def __str__(self):
         return self.name
 
-
 class MovieStar(models.Model):
-    name =  models.TextField()
+    name = models.TextField(db_index=True)  # Индекс на поле name
     def __str__(self):
         return self.name
 
 
 class Movie(models.Model):
-    movie_id = models.TextField()
-    movie_name = models.TextField()
-    description = models.TextField()
-
-    year = models.IntegerField()
+    movie_id = models.TextField(db_index=True)  # Индекс на поле movie_id
+    movie_name = models.TextField(db_index=True)  # Индекс на поле movie_name
+    rating = models.FloatField(db_index=True)  # Индекс на поле rating
+    
+    year = models.IntegerField(db_index=True)  # Индекс на поле year
     certificate = models.TextField()
     runtime = models.IntegerField()
 
